7a3013ef852f5e9308dec4fd2a9f46ba3565c9c2
april2901 <april2901@naver.com>
2025-05-22 23:24:53 +0900

    edit app

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import sqlite3
 app = Flask(__name__)
 
 def get_db():
-    conn = sqlite3.connect('wines.db')
+    conn = sqlite3.connect('wine.db')
     conn.row_factory = sqlite3.Row
     return conn
 
